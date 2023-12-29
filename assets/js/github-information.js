9d8ed5f0e60a91547b77cb084b0711e464b4dc37
roc-11 <>
2023-12-29 10:44:01 +0000

    Display loader on gihub page

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -1,1 +1,18 @@
 console.log("Github Info");
+
+function fetchGitHubInformation(event) {
+    var username = $("gh-username").val;
+
+    if (!username) {
+        $("gh-user-data").html(`<h2>Please enter a GitHub username</h2>`);
+        return;
+    }
+
+    // display a loader gif while data is being accessed
+    $("gh-user-data").html(
+        `<div id="loader">
+            <img src="assets/css/loader.gif" alt="loading...">
+        </div>`
+    );
+
+}
